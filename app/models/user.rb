4a5af3fe92acb6d668dd1c2f1bc8ec35ff866242
Stephen Chang <stephenycchang@gmail.com>
2014-05-31 22:16:45 -0400

    Defined first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ActiveRecord::Base
 has_many :products
 
 validates :name, presence: true
+
+def first_name
+  first_name = name.split(' ', 2).first
+end
+
+def last_name
+  last_name = name.match(" ").post_match
+end
 end
