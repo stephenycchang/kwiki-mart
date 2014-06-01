ea7fe41a42cc85635152d94b7a9523b92db53716
Stephen Chang <stephenycchang@gmail.com>
2014-06-01 16:57:50 -0400

    Added Cancan ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-has_many :products
+  has_many :products
 
-validates :name, presence: true
+  validates :name, presence: true
 
-def first_name
-  first_name = name.split(' ', 2).first
-end
+  def first_name
+    first_name = name.split(' ', 2).first
+  end
 
-def last_name
-  last_name = name.match(" ").post_match
-end
+  def last_name
+    last_name = name.match(" ").post_match
+  end
 end
