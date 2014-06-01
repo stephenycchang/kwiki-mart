4e2b90d5a7e96b495a25380894c5db238cd6666f
Stephen Chang <stephenycchang@gmail.com>
2014-05-31 22:40:25 -0400

    Testing AngularJS searchbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
+//= require angular-animate
+//= require angular-resource
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
