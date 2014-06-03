40b11889a3e294020b7613b02f49af46213571f6
Stephen Chang <stephenycchang@gmail.com>
2014-06-03 12:42:38 -0400

    Changed New Products Path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root 'products#index'
 
   get "about" => "pages#about"
+  get "new" => "products#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
